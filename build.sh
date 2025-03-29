86a7ffe481ee8c5201e30c04175df56a1042db41
Suryansh0707 <115163165+Suryansh0707@users.noreply.github.com>
2025-03-29 13:00:16 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ docker tag $JOB_NAME:$BUILD_ID suryansh07/$JOB_NAME:$BUILD_ID
 
 docker tag $JOB_NAME:$BUILD_ID suryansh07/$JOB_NAME:latest
 
-docker push jacksneel/$JOB_NAME:$BUILD_ID
+docker push suryansh07/$JOB_NAME:$BUILD_ID
 
-docker push jacksneel/$JOB_NAME:latest
+docker push suryansh07/$JOB_NAME:latest
 
 docker rmi -f $JOB_NAME:$BUILD_ID suryansh07/$JOB_NAME:$BUILD_ID suryansh07/$JOB_NAME:latest
